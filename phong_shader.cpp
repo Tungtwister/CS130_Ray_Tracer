82fcd0a2918db1bb292864d411f295f91f08479a
Austin Tung <austin.tung56@gmail.com>
2019-01-23 08:26:42 +0000

    ambient

--- phong_shader.cpp
+++ phong_shader.cpp
@@ -13,9 +13,16 @@ Shade_Surface(const Ray& ray,const vec3& intersection_point,
     vec3 colorA = {0,0,0}; //ambient
     vec3 colorD = {0,0,0}; //diffuse
     vec3 colorS = {0,0,0}; //specular
+    //L which is the direction vector from the point on the surface toward each light source ( {\displaystyle m} m specifies the light source),
+    //N which is the normal at this point on the surface,
+    //R which is the direction that a perfectly reflected ray of light would take from this point on the surface
+    //V which is the direction pointing towards the viewer (such as a virtual camera).
     
     //calulating ambient
-    colorA = this->world.ambient_color * this->world.ambient_intensity * this->color_ambient;
+    // Ia = Ra * La     0 <= Ra <= 1
+    vec3 La = world.ambient_color * world.ambient_intensity;
+    vec3 Ra = this->color_ambient;
+    colorA =  La * Ra;
     
     color = colorA + colorD + colorS;
     return color;
